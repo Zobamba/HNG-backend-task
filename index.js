27e37156034fe76d74ae4cbbd29e7298431622dc
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2023-09-07 14:05:33 +0100

    created an endpoint with two GET request query parameters that return specific information in JSON format

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.use(express.json());
+
+const port = 5000;
+
+// Homepage route
+app.get('/', (req, res) => {
+  res.send('Welcome To HNG Backend Stage One Task');
+});
+
+// Endpoint route
+
+app.get('/api', (req, res) => {
+  const { slack_name, track } = req.query;
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date();
+  const currentDay = days[date.getDay()];
+
+  res.status(200).send({
+    slack_name: slack_name,
+    current_day: currentDay,
+    utc_time: date.toISOString(),
+    track: track,
+    github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
+    github_repo_url: "https://github.com/Zobamba/HNG-backend-task",
+    status_code: 200,
+  });
+});
+
+app.listen(port, () => console.log(`index app listening on port ${port}!`));
